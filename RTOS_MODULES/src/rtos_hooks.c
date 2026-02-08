4e857308da76f77fbfaaec762a36f3285e68c6c5
mkpk <mikolajpieklo@gmail.com>
2026-02-08 09:09:44 +0100

    Add RTOS hooks.

---
+++ RTOS_MODULES/src/rtos_hooks.c
@@ -0,0 +1,113 @@
+/**
+ ********************************************************************************
+ * @file    rtos_hooks.c
+ * @author  Mikolaj Pieklo
+ * @date    08.02.2026
+ * @brief
+ ********************************************************************************
+ */
+
+/************************************
+ * INCLUDES
+ ************************************/
+#include <stdio.h>
+
+#include <FreeRTOS.h>
+#include <task.h>
+
+#ifdef STM32F103xB
+#include <stm32f1xx.h>
+#elif STM32F401xC
+#include <stm32f4xx.h>
+#endif
+
+/************************************
+ * EXTERN VARIABLES
+ ************************************/
+
+/************************************
+ * PRIVATE MACROS AND DEFINES
+ ************************************/
+
+/************************************
+ * PRIVATE TYPEDEFS
+ ************************************/
+
+/************************************
+ * STATIC VARIABLES
+ ************************************/
+
+/************************************
+ * GLOBAL VARIABLES
+ ************************************/
+
+/************************************
+ * STATIC FUNCTION PROTOTYPES
+ ************************************/
+
+/************************************
+ * STATIC FUNCTIONS
+ ************************************/
+
+/************************************
+ * GLOBAL FUNCTIONS
+ ************************************/
+void vApplicationMallocFailedHook(void)
+{
+   /* vApplicationMallocFailedHook() will only be called if
+    * configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+    * function that will get called if a call to pvPortMalloc() fails.
+    * pvPortMalloc() is called internally by the kernel whenever a task, queue,
+    * timer or semaphore is created.  It is also called by various parts of the
+    * demo application.  If heap_1.c or heap_2.c are used, then the size of the
+    * heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+    * FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+    * to query the size of free heap space that remains (although it does not
+    * provide information on how the remaining heap might be fragmented). */
+   taskDISABLE_INTERRUPTS();
+
+   printf("Malloc Failed!\r\n");
+   NVIC_SystemReset();
+
+   for (;;)
+   {
+   }
+}
+
+void vApplicationIdleHook(void)
+{
+   /* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+    * to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+    * task.  It is essential that code added to this hook function never attempts
+    * to block in any way (for example, call xQueueReceive() with a block time
+    * specified, or call vTaskDelay()).  If the application makes use of the
+    * vTaskDelete() API function (as this demo application does) then it is also
+    * important that vApplicationIdleHook() is permitted to return to its calling
+    * function, because it is the responsibility of the idle task to clean up
+    * memory allocated by the kernel to any task that has since been deleted. */
+}
+
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName)
+{
+   (void) pxTask;
+
+   /* Run time stack overflow checking is performed if
+    * configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+    * function is called if a stack overflow is detected. */
+   taskDISABLE_INTERRUPTS();
+
+   printf("STACK OVERFLOW! Task name = %s\r\n", pcTaskName);
+   // snprintf(, sizeof(), "STACK OVERFLOW! Task name = %s\r\n", pcTaskName);
+
+   NVIC_SystemReset();
+
+   for (;;)
+   {
+   }
+}
+
+void vApplicationTickHook(void)
+{
+}
